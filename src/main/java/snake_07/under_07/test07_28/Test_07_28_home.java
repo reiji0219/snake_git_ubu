d7030e6f5ce06d40782826e8c04756bab396935d
rei <e5nqm-git@yahoo.co.jp>
2025-08-03 14:12:23 +0900

    add_Item28

--- src/main/java/snake_07/under_07/test07_28/Test_07_28_home.java
+++ src/main/java/snake_07/under_07/test07_28/Test_07_28_home.java
@@ -2,11 +2,16 @@ public class Test_07_28_home
 {
   public static void main( String[] args )
   {
+    Item28.save( item -> {
+     item.id( "掛け算" )
+         .name( "税込み価格" )
+         .price( 980 )
+         .tax( 1.1 );
 
+    })
 
     System.out.println( "Hello World_1" );
     System.out.println( "Hello World_2" );
     System.out.println( "Hello World_3" );
-    System.out.println( "Hello World_4" );
   }
 }
