35bc86460735440044caf9c45798d55410a09cae
rei <e5nqm-git@yahoo.co.jp>
2025-08-06 14:21:21 +0900

    add_create

--- src/main/java/snake_07/under_07/test07_28/Calc28.java
+++ src/main/java/snake_07/under_07/test07_28/Calc28.java
@@ -7,13 +7,31 @@ public class Calc28 extends Item28
   public Calc28( String name ){
     super();
     this.name = name;
-   }
+  }
+
+  public static Interface create(){
+    return() -> {
+      Calc28 calc = new Calc28( "Poly-morphism" );
+
+      Item28.save( item -> {
+       item.id( "掛け算" )
+           .name( "税込み価格" )
+           .price( 980 )
+           .tax( 1.1 )
+           .calc( calc );
+
+        System.out.println( "計算式 :" + item.getId() );
+
+        item.display();
+      });
+    };
+  }
 
   public void display(){
     indi( name );
   }
 
-  public void display( Interface inter ){
+  public void process( Interface inter ){
     indi( "start" );
 
     inter.execute();
--- src/main/java/snake_07/under_07/test07_28/Test_07_28_home.java
+++ src/main/java/snake_07/under_07/test07_28/Test_07_28_home.java
@@ -1,25 +1,17 @@
+import demo_06.Interface;
+
 public class Test_07_28_home
 {
   public static void main( String[] args )
   {
     Calc28 calc = new Calc28( "Poly-morphism" );
-    calc.display( () -> {
+    calc.process( () -> {
       System.out.println( "関心事を記述" );
 
+      Interface save = Calc28.create();
+        save.execute();
+
+      System.out.println( "Hello World_1" );
     });
-
-    Item28.save( item -> {
-     item.id( "掛け算" )
-         .name( "税込み価格" )
-         .price( 980 )
-         .tax( 1.1 )
-         .calc( calc );
-
-      System.out.println( "計算式 :" + item.getId() );
-
-      item.display();
-    });
-
-    System.out.println( "Hello World_1" );
   }
 }
