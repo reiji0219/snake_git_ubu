36b5eac70ed5d040240e92493901064c76d4ed3e
rei <e5nqm-git@yahoo.co.jp>
2025-08-11 14:18:15 +0900

    Test_06_04_home を加筆しました

---
+++ src/main/java/snake_07/under_07/test06_04/Calc04.java
@@ -0,0 +1,44 @@
+ package main.java.snake_07.under_07.test06_04;
+
+import demo_06.Interface;
+
+public class Calc04 extends Item04
+{
+  private String name;
+
+  public Calc04( String name ){
+    super();
+    this.name = name;
+  }
+
+  public static Interface create(){
+    return() -> {
+      Calc04 calc = new Calc04( "Poly-morphism" );
+
+      Item04.save( item -> {
+        item.id( "掛け算" )
+            .name( "税込み価格" )
+            .price( 980 )
+            .tax( 1.1 )
+            .calc( calc );
+
+        System.out.println( "計算式 :" + item.getId() );
+
+        item.display();
+
+        item.execute();
+      });
+    };
+  }
+  public void display(){
+    indi( name );
+  }
+
+  public void process( Interface inter ){
+    indi( "start" );
+
+    inter.execute();
+
+    indi( "end" );
+  }
+}
