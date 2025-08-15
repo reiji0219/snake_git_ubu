05b13fe57c23446f0a9bec2c9fef1ead444e9a19
rei <e5nqm-git@yahoo.co.jp>
2025-08-15 23:47:22 +0900

    add_Test06_03.jar

---
+++ src/main/java/snake_07/under_07/test06_03/Item03.java
@@ -0,0 +1,27 @@
+public class Item03
+{
+  private Stirng id;
+  private Stirng id;
+  private double id;
+  private double id;
+
+
+  public Item03 id( Stirng id ){
+    this.id = id;
+    return this;
+  }
+
+  public Item03 id( Stirng id ){
+    this.id = id;
+    return this;
+  }
+
+  public Item03 id( Stirng id ){
+    this.id = id;
+    return this;
+  }
+  public Item03 id( Stirng id ){
+    this.id = id;
+    return this;
+  }
+}
---
+++ src/main/java/snake_07/under_07/test06_03/Test_06_03_home.java
@@ -0,0 +1,12 @@
+public class Test_06_03_home
+{
+  public static void main( String[] args )
+  {
+
+
+    System.out.println( "Hello World_1" );
+    System.out.println( "Hello World_2" );
+    System.out.println( "Hello World_3" );
+    System.out.println( "Hello World_4" );
+  }
+}
