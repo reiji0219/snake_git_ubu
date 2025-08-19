df94f33856efa22eef7a3f0cf049fa4c89135b55
rei <e5nqm-git@yahoo.co.jp>
2025-08-19 13:43:41 +0900

    add_Test06_09.java

---
+++ src/main/java/snake_07/under_07/test06_09/Test_06_09_home.java
@@ -0,0 +1,21 @@
+package main.java.snake_07.under_07.test06_09;
+
+public class Test_06_09_home
+{
+  public static void main( String[] args )
+  {
+    Item09.save( item -> {
+      item.id( "掛け算" )
+          .name( "税込み価格" )
+          .price( 980 )
+          .tax( 1.1 );
+
+      System.out.println( "計算式 :" + item.getId() );
+
+      item.display();
+    });
+
+    System.out.println( "Hello World_1" );
+    System.out.println( "Hello World_2" );
+  }
+}
