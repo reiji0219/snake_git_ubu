c501ee2a97e7f219c87cd6be9f5a7aeb973ef520
rei <e5nqm-git@yahoo.co.jp>
2025-08-22 07:07:29 +0900

    WIP: Save local changes

---
+++ src/main/java/snake_07/under_07/test06_12/Test_06_12_home.java
@@ -0,0 +1,14 @@
+package main.java.snake_07.under_07.test06_12;
+
+public class Test_06_12_home
+{
+  public static void main( String[] args )
+  {
+
+
+   System.out.println( "Hello World_1" );
+   System.out.println( "Hello World_2" );
+   System.out.println( "Hello World_3" );
+   System.out.println( "Hello World_4" );
+  }
+}
