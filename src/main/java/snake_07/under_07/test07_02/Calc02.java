7a086911e40c91896c4567bbf9305ddbf2079fcd
rei <e5nqm-git@yahoo.co.jp>
2025-08-21 16:42:45 +0900

    add_Test_07_02_home

---
+++ src/main/java/snake_07/under_07/test07_02/Calc02.java
@@ -0,0 +1,13 @@
+public class Calc02 extends Item02
+{
+  private String name;
+
+  public Calc02( String name ){
+    super();
+    this.name = name;
+  }
+
+  public void display(){
+    indi( name );
+  }
+}
