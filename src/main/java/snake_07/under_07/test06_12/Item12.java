cdcc6bf0311b76e36afc1befd51e5de75b8edce2
rei <e5nqm-git@yahoo.co.jp>
2025-08-21 14:45:22 +0900

    add_Test_06_12_home

---
+++ src/main/java/snake_07/under_07/test06_12/Item12.java
@@ -0,0 +1,42 @@
+package main/java/snake_07/under_07/test06_12/
+
+public class Item12
+{
+  private String id;
+  private String name;
+  private double price;
+  private double tax;
+
+
+  public Item12 id( String id ){
+    this.id = id;
+    return this;
+  }
+
+  public Item12 name( String name ){
+    this.name = name;
+    return this;
+  }
+
+  public Item12 price( double price ){
+    this.price = price;
+    return this;
+  }
+
+  public Item12 tax( double tax ){
+    this.tax = tax;
+    return this;
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+}
