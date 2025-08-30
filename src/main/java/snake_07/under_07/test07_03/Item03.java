d535b74359c78f562bc3c749af1bc5c97e1d341a
rei <e5nqm-git@yahoo.co.jp>
2025-08-30 17:54:56 +0900

    add_Test07_03.java

---
+++ src/main/java/snake_07/under_07/test07_03/Item03.java
@@ -0,0 +1,43 @@
+public class Item03
+{
+  private String id;
+  private String name;
+  private double price;
+  private double tax;
+
+
+  public Item03 id( String id ){
+    this.id = id;
+    return this;
+  }
+
+  public Item03 name( String name ){
+    this.name = name;
+    return this;
+  }
+
+  public Item03 price( double price ){
+    this.price = price;
+    return this;
+  }
+
+  public Item03 tax( double tax ){
+    this.tax = tax;
+    return this;
+  }
+
+  public static void 
+
+
+
+
+
+
+
+
+
+
+  public static void indi( String s0 ){
+    System.out.println( s0 );
+  }
+}
