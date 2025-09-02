5c7bc6d7a2479d8572e60fab148db84e34178543
rei <e5nqm-git@yahoo.co.jp>
2025-09-03 07:17:32 +0900

    test07_06.tar.gz

--- src/main/java/snake_07/under_07/test07_07/Test_07_07_home.java
+++ src/main/java/snake_07/under_07/test07_07/Test_07_07_home.java
@@ -1,6 +1,6 @@
-package main.java.snake_07.under_07.test07_06;
+package main.java.snake_07.under_07.test07_07;
 
-public class Test_07_06_home
+public class Test_07_07_home
 {
   public static void main( String[] args )
   {
