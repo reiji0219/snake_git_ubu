b7979dbf3652e17dcbe66d40a06645d1f0de0b75
rei <e5nqm-git@yahoo.co.jp>
2025-09-06 17:35:31 +0900

    add_Test06_22

---
+++ src/main/java/snake_07/under_07/test06_22/Test_06_22_home.java
@@ -0,0 +1,12 @@
+public class Test_06_22_home
+{
+  public static void main( String[] args )
+  {
+
+
+    System.out.println( "Hello World_1" );
+    System.out.println( "Hello World_2" );
+    System.out.println( "Hello World_3" );
+  }
+}
+
