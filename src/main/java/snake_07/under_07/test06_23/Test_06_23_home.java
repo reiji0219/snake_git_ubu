ae4ff427f4e42a9e9ed1b7b323b3d4e62c023536
rei <e5nqm-git@yahoo.co.jp>
2025-09-07 20:43:43 +0900

    test06_23.tar.gz

--- src/main/java/snake_07/under_07/test06_23/Test_06_23_home.java
+++ src/main/java/snake_07/under_07/test06_23/Test_06_23_home.java
@@ -1,4 +1,6 @@
-public class Test_06_22_home
+package main.java.snake_07.under_07.test06_23;
+
+public class Test_06_23_home
 {
   public static void main( String[] args )
   {
@@ -7,6 +9,6 @@ public class Test_06_22_home
     System.out.println( "Hello World_1" );
     System.out.println( "Hello World_2" );
     System.out.println( "Hello World_3" );
+    System.out.println( "Hello World_4" );
   }
 }
-
