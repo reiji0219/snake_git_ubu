7e8b224375277dff44e48ce5500164d186cda896
rei <e5nqm-git@yahoo.co.jp>
2025-09-09 19:46:03 +0900

    test06_25

---
+++ src/main/java/snake_07/under_07/test06_25/Test_06_25_home.java
@@ -0,0 +1,11 @@
+package main.java.snake_07.under_07.test06_25;
+
+public class Test_06_25_home
+{
+  public static void main( String[] args )
+  {
+
+
+    System.out.println( "Hello World" );
+  }
+}
