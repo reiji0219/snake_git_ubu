34506ad5088cd865cda1fc6c42e2c77cbec9dd67
rei <e5nqm-git@yahoo.co.jp>
2025-09-19 15:11:17 +0900

    add_Calc29_manifest

--- src/main/java/snake_07/under_07/test06_29/Item29.java
+++ src/main/java/snake_07/under_07/test06_29/Item29.java
@@ -1,14 +1,20 @@
 package main.java.snake_07.under_07.test06_29;
 
+import demo_06.Interface;
 import java.util.function.Consumer;
 
-public class Item29
+public class Item29 implements Interface
 {
+  public void execute(){
+    indi( "Lambda" );
+
+  }
+
   private String id;
   private String name;
   private double price;
   private double tax;
-
+  private Calc29 calc;
 
   public Item29 id( String id ){
     this.id = id;
@@ -30,18 +36,33 @@ public class Item29
     return this;
   }
 
+  public Item29 calc( Calc29 calc ){
+    this.calc = calc;
+    return this;
+  }
+
   public static void save( Consumer<Item29> con ){
     Item29 item = new Item29();
     con.accept( item );
     indi( "save :" + item );
   }
 
+  public String getId(){
+    return id;
+  }
 
+  public void display(){
+    indi( "表示内容 :" + name );
 
+    double calcInTax = calcTax( price, tax );
+      indi( "税込み価格は" + calcInTax + "円です" );
 
+    calc.display();
+  }
 
-
-
+  public static double calcTax( double price, double tax ){
+    return( price * tax );
+  }
 
   public String toString(){
     return( "Item29 :" + "id=" + id + " " + "name=" + name + " " + "price=" + price + " " + "tax=" + tax );
