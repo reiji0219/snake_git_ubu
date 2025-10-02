24e68c8d6d25acb41c223a48b24102a7f3d14b10
rei <e5nqm-git@yahoo.co.jp>
2025-10-02 18:11:02 +0900

    add_Test_06_30_home

---
+++ src/main/java/snake_07/under_07/test06_30/Item30.java
@@ -0,0 +1,42 @@
+package main.java.snake_07.under_07.test06_30;
+
+public class Item30
+{
+  private String id;
+  private String name;
+  private double price;
+  private double tax;
+
+
+  public Item30 id( String id ){
+    this.id = id;
+    return this;
+  }
+
+  public Item30 name( String name ){
+    this.name = name;
+    return this;
+  }
+
+  public Item30 price( double price ){
+    this.price = price;
+    return this;
+  }
+
+  public Item30 tax( double tax ){
+    this.tax = tax;
+    return this;
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+}
