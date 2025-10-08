d65159dd43334033fa0666d6a98eb5a8919de073
rei <e5nqm-git@yahoo.co.jp>
2025-10-08 15:49:52 +0900

    add_test07_20

--- src/main/java/snake_07/under_07/test06_30/Item30.java
+++ src/main/java/snake_07/under_07/test06_30/Item30.java
@@ -1,5 +1,7 @@
 package main.java.snake_07.under_07.test06_30;
 
+import java.util.function.Consumer;
+
 public class Item30
 {
   private String id;
@@ -28,6 +30,11 @@ public class Item30
     return this;
   }
 
+  public static void save( Consumer<Item30> con ){
+    Item30 item = new Item30();
+    con.accept( item );
+    indi( "save :" + item );
+  }
 
 
 
@@ -36,7 +43,11 @@ public class Item30
 
 
 
+  public String toString(){
+    return( "Item30 :" + "id=" + id + " " + "name=" + name + " " + "price=" + price + " " + "tax=" + tax );
+  }
 
-
-
+  public static void indi( String s0 ){
+    System.out.println( s0 );
+  }
 }
---
+++ src/main/java/snake_07/under_07/test07_18/Test_07_18_home.java
@@ -0,0 +1,14 @@
+package main.java.snake_07.under_07.test07_18;
+
+public class Test_07_18_home
+{
+  public static void main( String[] args )
+  {
+
+
+    System.out.println( "Hello World_1" );
+    System.out.println( "Hello World_2" );
+    System.out.println( "Hello World_3" );
+    System.out.println( "Hello World_4" );
+  }
+}
