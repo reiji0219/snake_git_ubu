d5a444fa1a1b045f7aa28e796a344e6a6314b062
rei <e5nqm-git@yahoo.co.jp>
2025-11-15 17:59:32 +0900

    Test_08_07_home.bk

--- src/main/java/snake_08/upper_08/test08_07/Test_08_07_home.java
+++ src/main/java/snake_08/upper_08/test08_07/Test_08_07_home.java
@@ -2,4 +2,13 @@ package main.java.snake_08.upper_08.test08_11;
 
 public class Test_08_07_home
 {
+  public static void main( String[] args )
+  {
+
+
+    System.out.println( "Hello World_1" );
+    System.out.println( "Hello World_2" );
+    System.out.println( "Hello World_3" );
+    System.out.println( "Hello World_4" );
+  }
 }
