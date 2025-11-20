983c9ef785523b1462b3dc3782079a710b29b0b7
rei <e5nqm-git@yahoo.co.jp>
2025-11-21 07:07:11 +0900

    Test_08_09_home.java

--- src/main/java/snake_08/upper_08/test08_09/Calc09.java
+++ src/main/java/snake_08/upper_08/test08_09/Calc09.java
@@ -1,5 +1,45 @@
 package main.java.snake_08.upper_08.test08_09;
 
-public class Calc09
+import demo_06.Interface;
+
+public class Calc09 extends Item09
 {
+  private String name;
+
+  public Calc09( String name ){
+    super();
+    this.name = name;
+  }
+
+  public static Interface create(){
+    return() -> {
+      Calc09 calc = new Calc09( "Poly-morphism" );
+
+      Item09.save( item -> {
+        item.id( "掛け算" )
+            .name( "税込み価格" )
+            .price( 980 )
+            .tax( 1.1 )
+            .calc( calc );
+
+        System.out.println( "計算式 :" + item.getId() );
+
+        item.display();
+
+        item.execute();
+      });
+    };
+  }
+
+  public void display(){
+    indi( name );
+  }
+
+  public void process( Interface inter ){
+    indi( "start" );
+
+    inter.execute();
+
+    indi( "end" );
+  }
 }
---
+++ src/main/java/snake_08/upper_08/test08_16/Test_08_16_home.java
@@ -0,0 +1,5 @@
+package main.java.snake_08.upper_08.test08_16;
+
+public class Test_08_16_home
+{
+}
