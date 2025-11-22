9c7d655704db41dc2f42d9a63aa7d2797940001a
rei <e5nqm-git@yahoo.co.jp>
2025-11-23 06:59:40 +0900

    add_Test_08_11_home.java

--- src/main/java/snake_08/upper_08/test08_11/Item11.java
+++ src/main/java/snake_08/upper_08/test08_11/Item11.java
@@ -1,5 +1,7 @@
 package main.java.snake_08.upper_08.test08_11;
 
+import java.util.function.Consumer;
+
 public class Item11
 {
   private String id;
@@ -28,7 +30,11 @@ public class Item11
     return this;
   }
 
-  public static void 
+  public static void save( Consumer<Item11> con ){
+    Item11 item = new Item11();
+    con.accept( item );
+    indi( "save :" + item );
+  }
 
 
 
@@ -37,7 +43,9 @@ public class Item11
 
 
 
-
+  public String toString(){
+    return( "Item11 :" + "id=" + id + " " + "name=" + name + " " + "price=" + price + " " + "tax=" + tax );
+  }
 
   public static void indi( String s0 ){
     System.out.println( s0 );
--- src/main/java/snake_08/upper_08/test08_11/Test_08_11_home.java
+++ src/main/java/snake_08/upper_08/test08_11/Test_08_11_home.java
@@ -4,7 +4,13 @@ public class Test_08_11_home
 {
   public static void main( String[] args )
   {
-    
+    Item11.save( item -> {
+      item.id( "掛け算" )
+          .name( "税込み価格" )
+          .price( 980 )
+          .tax( 1.1 );
+
+    });
 
 
     System.out.println( "Hello World_1" );
