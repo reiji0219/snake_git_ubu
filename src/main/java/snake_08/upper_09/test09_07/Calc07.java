08d3a41447a38da7fcd9877d72a750ce2482e8da
rei <e5nqm-git@yahoo.co.jp>
2026-01-11 07:13:56 +0900

    add_Test_09_07_home.java

--- src/main/java/snake_08/upper_09/test09_07/Calc07.java
+++ src/main/java/snake_08/upper_09/test09_07/Calc07.java
@@ -1,5 +1,15 @@
 package main.java.snake_08.upper_09.test09_07;
 
-public class Calc07
+public class Calc07 extends Item07
 {
+  private String name;
+
+  public Calc07( String name ){
+    super();
+    this.name = name;
+  }
+
+  public void display(){
+    indi( name );
+  }
 }
