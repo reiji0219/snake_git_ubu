b7fc01dfa6c6d70b5b51b9fbba42778d7a7e6be3
rei <e5nqm-git@yahoo.co.jp>
2026-01-15 23:02:04 +0900

    add_Test_09_13_home.java

--- src/main/java/snake_08/upper_09/test09_13/Calc13.java
+++ src/main/java/snake_08/upper_09/test09_13/Calc13.java
@@ -1,5 +1,15 @@
 package main.java.snake_08.upper_09.test09_13;
 
-public class Calc13
+public class Calc13 extends Item13
 {
+  private String name;
+
+  public Calc13( String name ){
+    super();
+    this.name = name;
+  }
+
+  public void display(){
+    indi( name );
+  }
 }
--- src/main/java/snake_08/upper_09/test09_13/Item13.java
+++ src/main/java/snake_08/upper_09/test09_13/Item13.java
@@ -1,5 +1,63 @@
 package main.java.snake_08.upper_09.test09_13;
 
+import java.util.function.Consumer;
+
 public class Item13
 {
+  private String id;
+  private String name;
+  private double price;
+  private double tax;
+
+
+  public Item13 id( String id ){
+    this.id = id;
+    return this;
+  }
+
+  public Item13 name( String name ){
+    this.name = name;
+    return this;
+  }
+
+  public Item13 price( double price ){
+    this.price = price;
+    return this;
+  }
+
+  public Item13 tax( double tax ){
+    this.tax = tax;
+    return this;
+  }
+
+  public static void save( Consumer<Item13> con ){
+    Item13 item = new Item13();
+    con.accept( item );
+    indi( "save :" + item );
+  }
+
+  public String getId(){
+    return id;
+  }
+
+  public void display(){
+    indi( "表示内容 :" + name );
+
+    double calcInTax = calcTax( price, tax );
+      indi( "税込み価格は" + calcInTax + "円です" );
+
+    
+  }
+
+  public static double calcTax( double price, double tax ){
+    return( price * tax );
+  }
+
+  public String toString(){
+    return( "Item13 :" + "id=" + id + " " + "name=" + name + " " + "price=" + price + " " + "tax=" + tax );
+  }
+
+  public static void indi( String s0 ){
+    System.out.println( s0 );
+  }
 }
