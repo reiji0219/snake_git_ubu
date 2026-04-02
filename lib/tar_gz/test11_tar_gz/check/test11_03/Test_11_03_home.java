905b4a1505549ec9b4fb22e6e82c27845483244f
rei <e5nqm-git@yahoo.co.jp>
2026-04-02 12:40:19 +0900

    add_Test_11_04_home

---
+++ lib/tar_gz/test11_tar_gz/check/test11_03/Test_11_03_home.java
@@ -0,0 +1,13 @@
+package main.java.snake_08.upper_10.test11_03;
+
+import demo_06.Interface;
+
+public class Test_11_03_home
+{
+  public static void main( String[] args )
+  {
+    Interface save = Calc03.create();
+      save.execute();
+
+  }
+}
--- src/main/java/snake_08/upper_11/test11_04/Calc04.java
+++
@@ -1,5 +0,0 @@
-package main.java.snake_08.upper_11.test11_04;
-
-public class Calc04
-{
-}
