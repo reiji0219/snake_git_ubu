26ce0183cd6edd81765ad3d4bc90f1cbd48d344e
rei <e5nqm-git@yahoo.co.jp>
2026-05-29 23:45:37 +0900

    fix_02_Test_11_09_home

--- src/main/java/snake_08/upper_11/test11_09/Item09.java
+++ src/main/java/snake_08/upper_11/test11_09/Item09.java
@@ -1,5 +1,53 @@
 package main.java.snake_08.upper_11.test11_09;
 
+import java.util.function.Consumer;
+
 public class Item09
 {
+  private String id;
+  private String name;
+  private double price;
+  private double tax;
+
+
+  public Item09 id( String id ){
+    this.id = id;
+    return this;
+  }
+
+  public Item09 name( String name ){
+    this.name = name;
+    return this;
+  }
+
+  public Item09 price( double price ){
+    this.price = price;
+    return this;
+  }
+
+  public Item09 tax( double tax ){
+    this.tax = tax;
+    return this;
+  }
+
+  public static void save( Consumer<Item09> con ){
+    Item09 item = new Item09();
+    con.accept( item );
+    indi( "save :" + item );
+  }
+
+
+
+
+
+
+
+
+  public String toString(){
+    return( "Item09 :" + "id=" + id + " " + "name=" + name + " " + "price=" + price + " " + "tax=" + tax );
+  }
+
+  public static void indi( String s0 ){
+    System.out.println( s0 );
+  }
 }
