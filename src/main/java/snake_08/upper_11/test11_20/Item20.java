375071431e0ab19784f441401009d30cf3aa6810
rei <e5nqm-git@yahoo.co.jp>
2026-06-21 23:37:54 +0900

    new_Test_11_20_home

--- src/main/java/snake_08/upper_11/test11_20/Item20.java
+++ src/main/java/snake_08/upper_11/test11_20/Item20.java
@@ -1,5 +1,68 @@
 package main.java.snake_08.upper_11.test11_20;
 
+import java.util.function.Consumer;
+
 public class Item20
 {
+  private String id;
+  private String name;
+  private double price;
+  private double tax;
+  private Calc20 calc;
+
+  public Item20 id( String id ){
+    this.id = id;
+    return this;
+  }
+
+  public Item20 name( String name ){
+    this.name = name;
+    return this;
+  }
+
+  public Item20 price( double price ){
+    this.price = price;
+    return this;
+  }
+
+  public Item20 tax( double tax ){
+    this.tax = tax;
+    return this;
+  }
+
+  public Item20 calc( Calc20 calc ){
+    this.calc = calc;
+    return this;
+  }
+
+  public static void save( Consumer<Item20> con ){
+    Item20 item = new Item20();
+    con.accept( item );
+    indi( "save :" + item );
+  }
+
+  public String getId(){
+    return id;
+  }
+
+  public void display(){
+    indi( "表示内容 :" + name );
+
+    double calcInTax = calcTax( price, tax );
+      indi( "税込み価格は" + calcInTax + "円です" );
+
+    calc.display();
+  }
+
+  public static double calcTax( double price, double tax ){
+    return( price * tax );
+  }
+
+  public String toString(){
+    return( "Item20 :" + "id=" + id + " " + "name=" + name + " " + "price=" + price + " " + "tax=" + tax );
+  }
+
+  public static void indi( String s0 ){
+    System.out.println( s0 );
+  }
 }
