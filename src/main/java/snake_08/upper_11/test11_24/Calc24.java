784574da4d403143c8a74b8a2d2c61ae9e9f3d96
rei <e5nqm-git@yahoo.co.jp>
2026-06-30 23:43:44 +0900

    new_Test_11_24_home

--- src/main/java/snake_08/upper_11/test11_24/Calc24.java
+++ src/main/java/snake_08/upper_11/test11_24/Calc24.java
@@ -1,5 +1,14 @@
 package main.java.snake_08.upper_11.test11_24;
 
-public class Calc24
+public class Calc24 extends Item24
 {
+  private String name;
+
+  public Calc24( String name ){
+    this.name = name;
+  }
+
+  public void display(){
+    indi( name );
+  }
 }
