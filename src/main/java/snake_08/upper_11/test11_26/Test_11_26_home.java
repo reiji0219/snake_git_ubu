a36d10b5c5c126fa4c460d92a856f0fc0f5e15d6
rei <e5nqm-git@yahoo.co.jp>
2026-07-03 23:06:29 +0900

    new_Test_11_26_home

--- src/main/java/snake_08/upper_11/test11_26/Test_11_26_home.java
+++ src/main/java/snake_08/upper_11/test11_26/Test_11_26_home.java
@@ -2,4 +2,20 @@ package main.java.snake_08.upper_11.test11_26;
 
 public class Test_11_26_home
 {
+  public static void main( String[] args )
+  {
+    Item26.save( item -> {
+      item.id( "掛け算" )
+          .name( "税込み価格" )
+          .price( 980 )
+          .tax( 1.1 );
+
+      System.out.println( "計算式   :" + item.getId() );
+
+      item.display();
+    });
+
+    System.out.println( "Hello World_1" );
+    System.out.println( "Hello World_2" );
+  }
 }
