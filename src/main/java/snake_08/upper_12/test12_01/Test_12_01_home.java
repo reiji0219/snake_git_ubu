2dc3cb299f25e8f55d56eee6b462637154beac35
rei <e5nqm-git@yahoo.co.jp>
2026-07-16 23:32:32 +0900

    fix_2_Test_12_01_home

--- src/main/java/snake_08/upper_12/test12_01/Test_12_01_home.java
+++ src/main/java/snake_08/upper_12/test12_01/Test_12_01_home.java
@@ -1,13 +1,14 @@
 package main.java.snake_08.upper_12.test12_01;
 
+import demo_06.Interface;
+
 public class Test_12_01_home
 {
   public static void main( String[] args )
   {
-
+    Interface save = Calc01.create();
+      save.execute();
 
     System.out.println( "Hello World_1" );
-    System.out.println( "Hello World_2" );
-    System.out.println( "Hello World_3" );
   }
 }
