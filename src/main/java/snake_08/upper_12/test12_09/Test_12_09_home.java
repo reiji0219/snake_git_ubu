badaf48e5b00ae0a4880c5efffc776b0f5e2a878
rei <e5nqm-git@yahoo.co.jp>
2026-08-14 23:23:03 +0900

    fix_Test_12_09_home

--- src/main/java/snake_08/upper_12/test12_09/Test_12_09_home.java
+++ src/main/java/snake_08/upper_12/test12_09/Test_12_09_home.java
@@ -4,11 +4,14 @@ public class Test_12_09_home
 {
   public static void main( String[] args )
   {
+    Calc09 calc = new Calc09( "Poly-morphismjr" );
+
     Item09.save( item -> {
       item.id( "掛け算" )
           .name( "税込み価格" )
           .price( 980 )
-          .tax( 1.1 );
+          .tax( 1.1 )
+          .calc( calc );
 
       System.out.println( "計算式   :" + item.getId() );
 
@@ -16,6 +19,5 @@ public class Test_12_09_home
     });
 
     System.out.println( "Hello World_1" );
-    System.out.println( "Hello World_2" );
   }
 }
