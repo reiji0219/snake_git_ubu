db3f0e2ae703f00f528ee7291bf851dbc9789628
rei <e5nqm-git@yahoo.co.jp>
2026-09-12 22:00:20 +0900

    final_Test_12_24_home

--- src/main/java/snake_08/upper_12/test12_24/Calc24.java
+++ src/main/java/snake_08/upper_12/test12_24/Calc24.java
@@ -1,5 +1,48 @@
 package main.java.snake_08.upper_12.test12_24;
 
-public class Calc24
+import demo_06.Interface;
+
+public class Calc24 extends Item24
 {
+  private String name;
+
+  public Calc24( String name ){
+    super();
+    this.name = name;
+  }
+
+  public static Interface create(){
+    return() -> {
+      Calc24 calc = new Calc24( "Poly-morphism" );
+      calc.process( () -> {
+        System.out.println( "関心事を記述" );
+
+        Item24.save( item -> {
+          item.id( "掛け算" )
+              .name( "税込み価格" )
+              .price( 980 )
+              .tax( 1.1 )
+              .calc( calc );
+
+          System.out.println( "計算式   :" + item.getId() );
+
+          item.display();
+
+          item.execute();
+        });
+      });
+    };
+  }
+
+  public void display(){
+    indi( name );
+  }
+
+  public void process( Interface inter ){
+    indi( "start" );
+
+    inter.execute();
+
+    indi( "end" );
+  }
 }
