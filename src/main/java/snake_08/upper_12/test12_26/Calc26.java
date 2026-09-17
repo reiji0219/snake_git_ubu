62797a404cb70cbca8c17f545ef30b900f55205a
rei <e5nqm-git@yahoo.co.jp>
2026-09-17 23:28:16 +0900

    final_Test_12_26_home

--- src/main/java/snake_08/upper_12/test12_26/Calc26.java
+++ src/main/java/snake_08/upper_12/test12_26/Calc26.java
@@ -1,5 +1,7 @@
 package main.java.snake_08.upper_12.test12_26;
 
+import demo_06.Interface;
+
 public class Calc26 extends Item26
 {
   private String name;
@@ -9,7 +11,38 @@ public class Calc26 extends Item26
     this.name = name;
   }
 
+  public static Interface create(){
+    return() -> {
+      Calc26 calc = new Calc26( "Poly-morphism" );
+      calc.process( () -> {
+        System.out.println( "関心事を記述" );
+
+        Item26.save( item -> {
+          item.id( "掛け算" )
+              .name( "税込み価格" )
+              .price( 980 )
+              .tax( 1.1 )
+              .calc( calc );
+
+          System.out.println( "計算式   :" + item.getId() );
+
+          item.display();
+
+          item.execute();
+        });
+      });
+    };
+  }
+
   public void display(){
     indi( name );
+  }
+
+  public void process( Interface inter ){
+    indi( "start" );
+
+    inter.execute();
+
+    indi( "end" );
   }
 }
--- src/main/java/snake_08/upper_12/test12_26/Test_12_26_home.java
+++ src/main/java/snake_08/upper_12/test12_26/Test_12_26_home.java
@@ -1,22 +1,14 @@
 package main.java.snake_08.upper_12.test12_26;
 
+import demo_06.Interface;
+
 public class Test_12_26_home
 {
   public static void main( String[] args )
   {
-    Calc26 calc = new Calc26( "Poly-morphism" );
-      calc.display();
-
-    Item26.save( item -> {
-      item.id( "掛け算" )
-          .name( "税込み価格" )
-          .price( 980 )
-          .tax( 1.1 );
-
-    });
+    Interface save = Calc26.create();
+      save.execute();
 
     System.out.println( "Hello World_1" );
-    System.out.println( "Hello World_2" );
-    System.out.println( "Hello World_3" );
   }
 }
